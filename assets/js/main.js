24688a25447cdc32b1505d19757954a9ede6e525
ahmed khouya <khouyahmad@gmail.com>
2023-05-19 19:41:46 +0200

    about section and header blur

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,12 @@ const linkAction = ()=>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >=50 ? header.classList.add('scroll-header')
+                        : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== EMAIL JS ===============*/
 
